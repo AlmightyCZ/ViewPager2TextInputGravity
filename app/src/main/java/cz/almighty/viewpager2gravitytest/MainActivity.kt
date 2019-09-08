2e810acd2cea623635e29c28c2b260352565be6d
Pavel Nedoma <pnedoma@gmail.com>
2019-09-08 08:28:07 +0200

    hideKeyboard example

--- app/src/main/java/cz/almighty/viewpager2gravitytest/MainActivity.kt
+++ app/src/main/java/cz/almighty/viewpager2gravitytest/MainActivity.kt
@@ -1,8 +1,12 @@
 package cz.almighty.viewpager2gravitytest
 
 
+import android.app.Activity
 import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
 import androidx.appcompat.app.AppCompatActivity
+import androidx.viewpager2.widget.ViewPager2
 import com.google.android.material.tabs.TabLayoutMediator
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -21,7 +25,24 @@ class MainActivity : AppCompatActivity() {
                 tab.text = tabsAdapter.getPageTitle(position)
             }).attach()
 
+        viewPager.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
+            override fun onPageSelected(position: Int) {
+                super.onPageSelected(position)
+                if (position != 1) {
+                    hideKeyboard()
+                }
+            }
+        })
     }
 
-
+    fun hideKeyboard() {
+        val imm = this.getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
+        //Find the currently focused view, so we can grab the correct window token from it.
+        var view = this.currentFocus
+        //If no view currently has focus, create a new one, just so we can grab a window token from it
+        if (view == null) {
+            view = View(this)
+        }
+        imm.hideSoftInputFromWindow(view.windowToken, 0)
+    }
 }
